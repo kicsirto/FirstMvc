8cfdb5435ea64738c5c2eec6610888e73028b2f4
Bessenyei Krisztián <bessenyei.krisztian@vereb.dc>
2019-09-27 16:38:52 +0200

    Add project files.

---
+++ FirstMvc/Controllers/FilmekController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstMvc.Models;
+
+namespace FirstMvc.Controllers
+{
+    public class FilmekController : Controller
+    {
+        // GET: Filmek
+        public ActionResult Random()
+        {
+
+
+            var film = new Film() { Cim = "Nincs ilyen" };
+            return View(film);
+            //return Content("szeretem a tejet");
+            //return HttpNotFound();
+            //return RedirectToAction("About","Home", new { oldal=1, rendezes= "cim"});
+
+
+        }
+
+        public ActionResult Szerkesztes(int id)
+        {
+            return Content($"id = {id}");
+
+
+        }
+
+        public ActionResult Index(int oldal, string rendezes)
+        {
+            return Content($"oldalszám: {oldal} | rendezés: {rendezes}");
+
+
+        }
+
+    }
+}
---
+++ FirstMvc/Models/Film.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstMvc.Models
+{
+    public class Film
+    {
+       public int ID { set; get; }
+        public string Cim { get; set; }
+
+        
+
+
+    }
+}
